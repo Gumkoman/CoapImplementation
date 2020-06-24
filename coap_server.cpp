50fd22c9450f27db00c84404db9dff8c82073c6c
Maciej Dabkowski <gumkoman@gmail.com>
2020-06-24 04:22:13 +0200

    wersja finalna

--- coap_server.cpp
+++ coap_server.cpp
@@ -2,15 +2,15 @@
 #include <math.h>
 #define PACKET_BUFFER_LENGTH        100
 #define UDP_SERVER_PORT 1234
-bool debug = true;
+
 byte MAC[] = {0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x01}; //MAC adres karty sieciowej, to powinno byc unikatowe - proforma dla ebsim'a
 unsigned int localPort = UDP_SERVER_PORT;
 
-bool coapServer::addNumber(int newNumber, int currentSize) {
-  this->zasob[currentSize] = newNumber;
+bool coapServer::addNumber(int newNumber, int currentSize) {//metoda przypisujaca otrzymany numer do zasobow serwera, przyjmuje nowa liczbe oraz miejsce zasobu
+  this->zasob[currentSize] = newNumber; //przypisanie do zasobow
 }
 
-bool isEqual(char* text1, char* text2, int textSize) {
+bool isEqual(char* text1, char* text2, int textSize) {//funkcja porownmujaca dwie tablice charow  
   for (int i = 0; i < textSize; i++) {
     if (text1[i] != text2[i]) {
       return false;
@@ -19,14 +19,14 @@ bool isEqual(char* text1, char* text2, int textSize) {
   }
 
 }
-int gcd(int a, int b) 
+int gcd(int a, int b) //funkcja wyliczajca warotosc nwd dla 2 liczb
 { 
     if (a == 0) 
         return b; 
     return gcd(b % a, a); 
 }
 
-int findlcm(int arr[], int n) 
+int findlcm(int arr[], int n) // funkcja wyliczajaca nww dla tablicy intow
 { 
     // Initialize result 
     int ans = arr[0]; 
@@ -40,7 +40,7 @@ int findlcm(int arr[], int n)
     return ans; 
 }
 
-int findGCD(int arr[], int n) 
+int findGCD(int arr[], int n) // funkcja wyliczajaca nwd dla tablicy intow
 { 
     int result = arr[0]; 
     for (int i = 1; i < n; i++) 
@@ -54,7 +54,7 @@ int findGCD(int arr[], int n)
     } 
     return result; 
 } 
-char * toArray(unsigned int number) {
+char * toArray(unsigned int number) { //funkcja pomocnicza zmianiajaca zmienna int na tablice znakow
     int length = (int)floor(log10((float)number)) + 1;
     char * arr = new char[length];
     int i = 0;
@@ -65,7 +65,7 @@ char * toArray(unsigned int number) {
     } while (number != 0);
     return arr;
 }
-bool coapServer::start() {
+bool coapServer::start() { // funkcja inicjalizujaca polaczenie udp
 
   ObirEthernet.begin(MAC);
   Serial.print(F("My IP address: "));
@@ -75,39 +75,37 @@ bool coapServer::start() {
   Serial.println();
   Udp.begin(localPort);
 }
-bool coapServer::loop() {
-  //Serial.println("Petelka kurwy");
-  int packetSize = Udp.parsePacket();
-  if (packetSize > 0) {
-    Serial.println("Witam udp ty kurwo jebana");
-    int len = Udp.read(packetBuffer, PACKET_BUFFER_LENGTH);
-    Serial.print("Recieved: ");
+bool coapServer::loop() { // glowna petla serwera coap
+  int packetSize = Udp.parsePacket(); //sprawdzenie czy otrzymano pakiet udp
+  if (packetSize > 0) { // jezeli odebrano pakiet udp
+    int len = Udp.read(packetBuffer, PACKET_BUFFER_LENGTH); // odczytywanie pakietu
+    Serial.print("Recieved: "); //wypisywanie otrzymanego pakietu
     packetBuffer[len] = '\0';
     packetMessage[len] = '\0';
     Serial.println((char*)packetBuffer);
-    coapPacket cPacket;
-    cPacket.coapVersion = packetBuffer[0] & 196 >> 6;
-    cPacket.type = packetBuffer[0] & 48 >> 4;
-    cPacket.tokenlen = packetBuffer[0] & 15;
-    cPacket.code = packetBuffer[1];
-    cPacket.messageId = (packetBuffer[2] << 8) | packetBuffer[3];//łaczenie dwój bajtów
+    coapPacket cPacket; //tworzenie obiektu klasy coapPacket
+    cPacket.coapVersion = packetBuffer[0] & 196 >> 6; //odczytywanie wersji coapa z wiadomosci
+    cPacket.type = packetBuffer[0] & 48 >> 4;//odczytywanie typu otrzymanej wiadomosci 
+    cPacket.tokenlen = packetBuffer[0] & 15;//odczytywanie TKL wiadomosci
+    cPacket.code = packetBuffer[1]; //odczytanie kodu otrzymanej wiadomosci
+    cPacket.messageId = (packetBuffer[2] << 8) | packetBuffer[3];//łaczenie dwóch bajtów
     //obsluga tokena
     if (cPacket.tokenlen > 0) {
       uint8_t token = new uint8_t[cPacket.tokenlen];
     } else {
       uint8_t token = NULL;
-    }//dodac czytanie tokena xd casme moze byc inny niz 0
+    }
     for (int i = 0; i < cPacket.tokenlen; i++) {
       cPacket.token[i] = packetBuffer[4 + i];
     }
-    //obsługa opcji
-    int optionNumber = 0;
-    bool isNextOption = true;
-    int currentByte = 4 + cPacket.tokenlen;
+    //obsługa opcji w zalozeniu ograniczamy liczbe opcji oraz tworzymy tablice je przechowujaca
+    int optionNumber = 0;// liczba porzadkowa opcji pomocna w operacjach na tablicach
+    bool isNextOption = true; // flaga sprawdzajaca istnienie kolejnej opcji do odczytania
+    int currentByte = 4 + cPacket.tokenlen; // przypisanie aktualnie zapisywanego bitu, zabieg konieczny przy obslugiwaniu kilku opcji
     while (isNextOption) {
-      cPacket.cOption[optionNumber].delta = (packetBuffer[currentByte] & 244 ) >> 4;
-      cPacket.cOption[optionNumber].optionLength = packetBuffer[currentByte] & 15;
-      //handling extended option and lnght
+      cPacket.cOption[optionNumber].delta = (packetBuffer[currentByte] & 244 ) >> 4;//przypisanie delty/length w option
+      cPacket.cOption[optionNumber].optionLength = packetBuffer[currentByte] & 15; 
+      //obsluga rozszerzonych opcji i rozszerzonej delty ( extended )
       if (cPacket.cOption[optionNumber].delta == 13) {
         currentByte++;
         cPacket.cOption[optionNumber].delta += packetBuffer[currentByte];
@@ -117,6 +115,7 @@ bool coapServer::loop() {
         cPacket.cOption[optionNumber].optionLength += packetBuffer[currentByte];
       }
       currentByte++;
+      //zapisanie wartosci opcji
       if (cPacket.cOption[optionNumber].optionLength > 0) {
         cPacket.cOption[optionNumber].optionValue = new uint8_t[cPacket.cOption[optionNumber].optionLength];
         for (int i = 0 ; i < cPacket.cOption[optionNumber].optionLength; i++) {
@@ -124,7 +123,7 @@ bool coapServer::loop() {
           currentByte++;
         }
       }
-      //checking if there are more options
+      //sprawdzenie czy nie ma wiecej opcji
       if (packetBuffer[currentByte] == NULL) {
         isNextOption = false;
       }
@@ -134,76 +133,60 @@ bool coapServer::loop() {
       optionNumber++;
     }
 
-    //handling payload
-    uint8_t payloadMarker = 255;
+    //obsluga payload
+    uint8_t payloadMarker = 255; // przypisanie payloadmarkera 
     if (packetBuffer[currentByte] == payloadMarker) {
       int nextByte = currentByte;
       while (packetBuffer[nextByte] != NULL) {
-        nextByte++;
+        nextByte++; // wyliczenie dlugosci payloadu na podstawie dlugosci pakietu udp i aktualnie odczytywanego bitu
       }
-      cPacket.payload = new uint8_t[nextByte - currentByte - 1];
+      cPacket.payload = new uint8_t[nextByte - currentByte - 1]; // zadeklarowanie dlugosci payloadu na podtawie utworzonej wczesniej dlugosci
       for (int i = 0; i < (nextByte - currentByte - 1); i++) {
-        cPacket.payload[i] = packetBuffer[i + currentByte + 1];
+        cPacket.payload[i] = packetBuffer[i + currentByte + 1];//zapisanie payloadu
 
       }
 
 
 
     }
-    /*Serial.print("coapver ");
-      Serial.println(cPacket.coapVersion);
-      Serial.print("code ");
-      Serial.println(cPacket.code);
-      Serial.print("tokenlen ");
-      Serial.println(cPacket.tokenlen);
-      Serial.print("type ");
-      Serial.println(+cPacket.type);
-      Serial.print("messageId ");
-      Serial.println(cPacket.messageId);
-      Serial.println();*/
-    //handeled recived message now givew a response
+
+    //obsluga otrzymanych pakietow i odpowiedz
     if (cPacket.code == 1) {
       if (isEqual(".well-known", cPacket.cOption[0].optionValue, 11)) {
-        if (isEqual("core", cPacket.cOption[1].optionValue, 4)) {
+        if (isEqual("core", cPacket.cOption[1].optionValue, 4)) {//przypadek w ktorym otrzymujemy uri-path - ./well-known/core
           cPacket.response.coapVersion = cPacket.coapVersion;
           uint8_t response[100] = {32};
-          response[0] = cPacket.coapVersion << 6 | 4 << 4 | cPacket.tokenlen;
-          response[1] = 69;
+          response[0] = cPacket.coapVersion << 6 | 4 << 4 | cPacket.tokenlen; //przypisanie wersji z oczytanego pakietu
+          response[1] = 69; //response code 2.05
           response[2] = cPacket.messageId >> 8;
           response[3] = cPacket.messageId;
           int currentByte = 4;
-          response[currentByte] = 194;//
+          response[currentByte] = 194;//przypisanie wartosci delta i lenght wysylanej opcji
           currentByte++;
-          response[currentByte] = 0;
+          response[currentByte] = 0;//przypisanie wartosci opcji
           currentByte++;
-          response[currentByte] = 40;
+          response[currentByte] = 40;//przypisanie wartosci opcji
           currentByte++;
-          response[currentByte] = 255;
-          char payload[] = "</zbior>;if=zbior;</metryka1>;if=metryka1;</metryka2>;if=metryka2;</metryka3>;if=metryka3;";
+          response[currentByte] = 255; // payload marker
+          char payload[] = "</zbior>;if=zbior;</metryka1>;if=metryka1;</metryka2>;if=metryka2;</metryka3>;if=metryka3;";//zasob well-known/core
 
           currentByte++;
           for (int i = 0; i < sizeof(payload); i++) {
-            response[currentByte] = payload[i];
+            response[currentByte] = payload[i];//przypisanie payloadu do wysylanej odpowiedzi
             currentByte++;
           }
-          Serial.println("Pokaz wiadmosci");
-          for (int i = 0; i < sizeof(response); i++) {
-            Serial.print(response[i]);
-            Serial.print(" ");
-          }
-          free(payload);
-          Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
-          Udp.write(response, sizeof(response));
-          Udp.endPacket();
-          free(response);
+          
+          Udp.beginPacket(Udp.remoteIP(), Udp.remotePort()); //odczytanie ip i portu do ktorego wysylamy odpowiedz
+          Udp.write(response, sizeof(response)); //wysylanie odpowiedzi udp
+          Udp.endPacket(); //zakonczenie sesji udp
         }
       }
 
-      if (isEqual("zbior", cPacket.cOption[0].optionValue, 5)) {
+      if (isEqual("zbior", cPacket.cOption[0].optionValue, 5)) { //uripath = zbior
         //get zbior
         Serial.println("zbior");
         byte response1[100];
-        response1[0] = cPacket.coapVersion << 6 | 4 << 4 | cPacket.tokenlen;
+        response1[0] = cPacket.coapVersion << 6 | 4 << 4 | cPacket.tokenlen; //przypisywanie wartosci zgodnie z poprzednim schematem
         response1[1] = 69;
         response1[2] = cPacket.messageId >> 8;
         response1[3] = cPacket.messageId;
@@ -221,23 +204,18 @@ bool coapServer::loop() {
         response1[currentByte] = 0b00000000;
         currentByte++;
 
-
-        //        response1[currentByte] = 0;
-        //        currentByte++;
-        //char *payload1;
-        char payload1[20] = {(char)32} ;
-        Serial.print("adsad");
-        Serial.println(cPacket.cOption[1].delta);
+        char payload1[20];
+        Serial.println(cPacket.cOption[1].delta); //sprawdzenie nww i nwd przed wyslaniem(do naszego wgladu w konsoli)
         if (cPacket.cOption[1].delta == 6) {
           Serial.println("NWW NWD");
           int zasobLen = 0;
-            while (this->zasob[zasobLen] != NULL) {
+            while (this->zasob[zasobLen] != NULL) {//jesli element zasobu jest NULL - > zwieksz
               zasobLen++  ;
             }
-          int nwd = findGCD(this->zasob,zasobLen);
+          int nwd = findGCD(this->zasob,zasobLen); //obliczanie nwd na podstawie elementow tablicy zasob
           Serial.print("nwd");
           Serial.println(nwd);
-          int nww = findlcm(this->zasob,zasobLen);
+          int nww = findlcm(this->zasob,zasobLen); //obliczanie nww j.w.
           //int nww = 
           payload1[0]='n';
           payload1[1]='w';
@@ -248,11 +226,11 @@ bool coapServer::loop() {
           payload1[6]='w';
           payload1[7]='w';
           payload1[8]=' ';
-          char *nwwchar = toArray(nww);
+          char *nwwchar = toArray(nww); //funkcja zamieniajaca int na char
           Serial.print("NWW");
           Serial.println(nww);
           for(int i=0;i<sizeof(nwwchar);i++){
-            payload1[9+i]=nwwchar[sizeof(nwwchar)-i-1]+48;
+            payload1[9+i]=nwwchar[sizeof(nwwchar)-i-1]+48; //przypisanie odpowiedzi do payloadu
             Serial.println(nwwchar[i]);  
           } 
         } else if (cPacket.token == 0) {
@@ -303,7 +281,7 @@ bool coapServer::loop() {
 
 
 
-    if (cPacket.code == 3) {
+    if (cPacket.code == 3) { //obsluga  zadania PUT
       Serial.println("PUT");
       if (isEqual("zbior", cPacket.cOption[0].optionValue, 5)) {
         int payloadSize = cPacket.cOption[1].optionValue;
@@ -336,7 +314,7 @@ bool coapServer::loop() {
             currentByte++;
           }
         }
-        //adding to array
+        //dodawanie do tablicy
         if (isSpaceFlag) {
           int newNumber;
           newNumber = cPacket.payload[0];
@@ -346,7 +324,7 @@ bool coapServer::loop() {
           Serial.println();
           this->addNumber(newNumber, zasobLen);
         }
-        Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+        Udp.beginPacket(Udp.remoteIP(), Udp.remotePort()); //przetworzenie i wysylka pakietu udp
         Udp.write(response, sizeof(response));
         Udp.endPacket();
       }
